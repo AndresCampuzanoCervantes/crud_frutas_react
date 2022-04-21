6e301c87ae9e0ba62fa76869190990fa8653185c
Andres Felipe Campuzano Cervantes <andrescampu1@gmail.com>
2022-04-20 19:43:42 -0500

    correccion de warnings

--- src/App.js
+++ src/App.js
@@ -1,11 +1,4 @@
-import logo from './logo.svg';
 import './App.css';
-import Parrafo from './components/Parrafo'
-import Variables from './components/Variables'
-import Contador from './components/Contador';
-import Eventos from './components/Eventos';
-import Listas from './components/Listas'
-import PokeApi from './components/PokeApi';
 import Formulario from './components/Formulario';
 function App() {
 
